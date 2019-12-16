7009a5c9a58fe4892a27ba031d3f72dc183a567f
penelopeom <penelopeom@aol.com>
2019-12-16 08:39:50 -0500

    Pushing three and final worlds

---
+++ Sarah_world.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Sarah_world here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sarah_world extends World
+{
+
+    /**
+     * Constructor for objects of class Sarah_world.
+     * 
+     */
+    public Sarah_world()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
