0afc18cf1bd9b4fedd40686b555cbd417eee8b8d
penelopeom <penelopeom@aol.com>
2019-12-19 08:50:13 -0500

    first day totally productive

---
+++ Blob.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class kingBlob here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Blob extends Actor
+{
+    /**
+     * Act - do whatever the kingBlob wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        movie();
+    }    
+    
+    public void movie() {
+        if (Greenfoot.isKeyDown("right") && getX() < ((Elmos_World)getWorld()).getWidth() - (getImage().getWidth() / 2)) {
+            setLocation(getX()+3, getY());
+        }
+        if (Greenfoot.isKeyDown("left") && getX() > (getImage().getWidth()) / 2) {
+            setLocation(getX()-3, getY());
+        }
+        if (Greenfoot.isKeyDown("up") && getY() > (getImage().getHeight()) / 2) {
+            setLocation(getX(), getY()-3);
+        }
+        if (Greenfoot.isKeyDown("down") && getY() < ((Elmos_World)getWorld()).getHeight() - (getImage().getHeight() / 2)) {
+            setLocation(getX(), getY()+3);
+        }
+    }
+}
